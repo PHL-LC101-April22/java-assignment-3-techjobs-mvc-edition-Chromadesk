0ec28fba7e444c9f4f5d1a99ccba26bf7720398b
Chromadesk <yisraeleliyah@gmail.com>
2022-09-20 22:33:49 -0400

    Completed assignment, may have to tweak autograder in order to pass tests.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,10 +1,14 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -19,12 +23,15 @@ public class SearchController {
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", new ArrayList<String>());
         return "search";
     }
 
     @PostMapping("results")
-    public String displaySearchResults(Model model) {
-
+    public String displaySearchResults(@RequestParam String searchTerm, String searchType, Model model) {
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", JobData.findByColumnAndValue(searchType, searchTerm));
+        return "search";
     }
 
 }
